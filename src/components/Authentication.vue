9d3b48469256b87eabc55d1956fc44be2fcf673f
austin <austin89peterson@gmail.com>
2018-12-04 11:12:20 -0700

    firebase

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,14 +1,31 @@
 <template>
-    <div>
-        <h3>Have fun Austin</h3>
+  <div id="login">
+    <h3>login</h3>
+    <v-btn flat href="/login" target="login here">
+     login
+    </v-btn>
+    <div component="dashboard">
+        <ul>
+      <li v-for="user in users" v-bind:key="user.id">
+        id: {{user.id}}
+     
+      </li>
+    </ul>
     </div>
+  </div>
 </template>
 
+
 <script>
-    export default {
-      name: 'authentication',
-      data: {
-          
-      }
-    }
+const db = firebase.firestore();
+console.log(db);
+export default {
+  name: "login",
+  data: () => ({ users: []
+   }),
+  firestore: {
+    users: db.collection("users"),
+    
+  }
+};
 </script>
